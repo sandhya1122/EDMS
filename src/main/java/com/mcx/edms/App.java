080bc2cca2bb7d1b0bd48e435f72deae401d86c5
sandhya1122 <sandhyaputtur19993@gmail.com>
2017-11-27 13:57:18 +0530

    Import Source Code

---
+++ src/main/java/com/mcx/edms/App.java
@@ -0,0 +1,13 @@
+package com.mcx.edms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
